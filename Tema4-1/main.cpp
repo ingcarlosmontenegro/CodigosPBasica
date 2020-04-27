28d1d016a2d78be6e2307e7e8b6204a40b3776ec
Carlos Montenegro <carlos.montenegro@unir.net>
2020-04-27 21:47:51 +0000

    cambiar el progrmama 4-1

--- Tema4-1/main.cpp
+++ Tema4-1/main.cpp
@@ -8,11 +8,12 @@ int main(int argc, char **argv)
     int b;
     cout << "ingrese valor para a: ";
     cin >> a;
-     cout << "ingrese valor para a: ";
+     cout << "ingrese valor para b: ";
     cin >> b;
     int S = a + b;
     int P = a *b;
 	cout << "a+b=" << S << "\n";
   cout << "a*b=" << P << "\n";
+  cout << "FIN \n ";
 	return 0;
 }
